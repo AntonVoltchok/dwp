ef8653473e859cad226e647a00512730ddba3e2d
Anton Voltchok <antonvolt@antons-mbp.mynetworksettings.com>
2025-11-12 13:47:06 -0500

    added content file

---
+++ src/utils/content.js
@@ -0,0 +1,236 @@
+export const content = {
+    title: "De Waal Psychology",
+    subtitle: "Psychotherapy and Assessment Services",
+    quote: "WE MAY NOT BE RESPONSIBLE FOR THE WORLD THAT CREATED OUR MINDS, BUT WE CAN TAKE RESPONSIBILITY FOR THE MIND WITH WHICH WE CREATE OUR WORLD.",
+    quoteAuthor: "Dr. Gabor Mate",
+    description: "Welcome to De Waal Psychology. I work with individuals through a holistic and creative therapeutic lens. I aspire to develop a connection with my clients by way of curiosity and compassion in order to build trust and enhance clients' unique sense of self. With an integrative approach to treatment, I utilize components from several different therapeutic modalities in order to meet the unique needs of the individual. In addition to psychotherapy, De Waal Psychology offers an array of services, including intensive trauma-informed treatment, diagnostic and forensic assessment and consultation.",
+    services: [
+        {
+            title: "Individual therapy",
+            description: "Individual therapy is a process that involves resolving behaviors, beliefs, feelings, or any other issues that cause distress. Through individual therapy a person can change distressful patterns of behavior or habits, decrease painful feelings, and improve their relationships."
+        },
+        {
+            title: "Supervision",
+            description: "I provide clinical supervision as a way to help therapists develop their skills, reflect on their work, and improve the overall well being as a person and a therapist. Supervision can be a helpful source of support while navigating the intricacies of a client therapy caseload. I also provide supervision for doctoral or masters level students looking to obtain clinical hours towards licensure."
+        },
+        {
+            title: "Forensic assessment",
+            description: "Assessment and consultation services to attorneys seeking guidance on their clients’ mental health needs. Evaluations are available specific to the needs of the client and may include, but are not limited to, mental health diagnostic screening, treatment planning,  and violence risk assessment."
+        },
+        {
+            title: "Psychodiagnostic assessment",
+            description: "Diagnostic evaluations for individuals who are seeking specific clinical recommendations for treatment. Diagnostic evaluations may also be provided for individuals seeking workers compensation, family and medical leave (FMLA), or psychiatric disability services."
+        }
+    ],
+    treatment: [
+        {
+            title: "Psychodynamic Therapy",
+            description: "Psychodynamic therapy is a type of psychotherapy that integrates past experiences with present behavior, offering a comprehensive understanding of an individual’s life history. Over the years, psychodynamic therapy has evolved to emphasize the role of past unresolved conflicts and how they manifest themselves in shaping people's behavior and personality. It has shown to be effective in helping people deal with depression, anxiety, pain, and relationship issues."
+        },
+        {
+            title: "Dialectical Behavior Therapy (DBT)",
+            description: "DBT is a helpful form of therapy for individuals who experience dysregulation or intense emotions. This modality of therapy focuses on acceptance and changing maladaptive or self destructive behaviors and is based on four key pillars- mindfulness, distress tolerance, emotion regulation, and interpersonal effectiveness."
+        },
+        {
+            title: "Cognitive Behavioral Therapy (CBT)",
+            description: "CBT can help a person become more aware of inaccurate or negative thinking in order to view challenging situations more clearly and respond to them in a more effective way. CBT is a collaborative, goal-oriented, and hands-on approach that focuses on the present and finding solutions to current problems."
+        },
+        {
+            title: "Art therapy",
+            description: "Engaging in creative expression can access a different part of our brain that encourages growth, awareness, and healing. Art therapy may be utilized to address specific concerns, but the act of creating is therapeutic in itself. Using creative tools in therapy provides a safe and grounding way to express emotions, allowing for deeper self understanding."
+        },
+        {
+            title: "Play therapy",
+            description: "Play therapy can be an effective form of treatment when working with children. It is a type of therapy that uses play and creative activities to help children express their thoughts and emotions, and work through challenges."
+        },
+        {
+            title: "EMDR",
+            description: "Eye Movement Desensitization and Reprocessing (EMDR) is an integrative psychotherapy approach that has been extensively researched and proven effective for the treatment of trauma. I offer EMDR if deemed appropriate in individual therapy or as a limited series of sessions while working with an outside therapist. To learn more about EMDR, visit www.emdria.org."
+        }
+    ],
+    aboutMe: {
+        firstPart: "I work from an attachment based framework to help identify causes of distress and increase awareness into disruptive patterns or problematic behaviors. Every person deserves to live a life that feels most fulfilling to them. We strive for happiness but sometimes old ways of thinking or behavioral patterns get in the way. We may feel stuck or find ourselves living a life based on the expectations of others or ways that do not feel authentic to our inner being.",
+        secondPart: "My speciality involves working with trauma, which is not always limited to a single catastrophic event that happened to us. It can also be a number of smaller, less pronounced events, experienced in early childhood or throughout the lifespan. Trauma is what happens inside of us rather than to us and it is what we continue to carry along the way. The weight may become lighter with gained awareness and the willingness to acknowledge it. Any level of trauma has the possibility of impacting the way we perceive the world or maintain relationships in adulthood. With therapy, this can be worked through by means of identifying a specific impactful event or series of events and acknowledging and working through the feelings associated with the trauma.",
+        thirdPart: "In working with children and teens, I focus on strength-based therapy to help identify causes of distress and increase problem-solving abilities and healthy coping strategies. This can improve communication skills and help regulate difficult or overwhelming emotions. I incorporate art therapy and play therapy as both are non-verbal techniques to process difficult feelings and experiences. I also specialize in working with families as every family goes through challenges as a unit and as children reach different developmental stages. Both personally and clinically, I enjoy art, music and mindfulness and incorporate them into my work as an entryway to process difficult emotions and to help self-soothe the body and mind."
+    },
+    backgroundAndExperience: {
+        firstPart: "I obtained my Doctorate in Clinical Psychology from Yeshiva University Albert Einstein College of Medicine, where I trained in various settings, including an acute psychiatric inpatient unit and psychiatric emergency room at Kings County Medical Center in Brooklyn, NY. My training also consisted of working with teens and young adults experiencing prodromal and acute symptoms of psychosis. In addition, I completed my Doctoral internship at a state psychiatric facility (Bronx Psychiatric Center) providing psychotherapy to individuals with severe and persistent mental illness. I performed research in the field of schizophrenia, including early psychosis and intervention (research publication).",
+        secondPart: "Prior to my doctoral training, I received my Master’s degree in Psychology at The New School for Social Research where I performed psychological research and received intensive training in addictions counseling. I’ve worked with individuals with substance use disorders, working as an intensive outpatient therapist performing DBT and group therapy focusing on relapse prevention and recovery.",
+        thirdPart: "Previous work experience also involves working in the field of forensic psychology, performing diagnostic and violence risk assessments. I held a position as Clinical Director for a non-profit organization, alongside the Manhattan District Attorney’s office, assessing inmates with severe mental illness in the criminal justice system. I continue to perform forensic evaluations to enhance clients’ ability to receive the treatment they need as opposed to continued incarceration."
+    },
+    resources: {
+        general: [
+            {
+                linkName: "Suicide Prevention Center",
+                link: "https://www.suicidepreventionlifeline.org/"
+            },
+            {
+                linkName: "Alcoholics Anonymous",
+                link: "https://www.aa.org/"
+            },
+            {
+                linkName: "Al-Anon",
+                link: "https://www.al-anon.org/"
+            },
+            {
+                linkName: "Anorexics and Bulimics Anonymous",
+                link: "https://aba12steps.org/"
+            },
+            {
+                linkName: "CoDA",
+                link: "https://coda.org/"
+            },
+            {
+                linkName: "National Domestic Violence Hotline",
+                link: "https://www.thehotline.org/"
+            },
+            {
+                linkName: "No Silence No Violence",
+                link: "https://www.speakingoutnsnv.org/"
+            },
+            {
+                linkName: "Treatment Facilities Finder",
+                link: "https://findtreatment.samhsa.gov/"
+            }
+        ],
+        podcasts: [
+            {
+                title: "Attachment Theory",
+                link: "https://open.spotify.com/episode/0wn4qnXF75GdrxX5013LbZ?si=sNb0Csm6TsiB66ezRTWjFw"
+            },
+            {
+                title: "Explaining Trauma",
+                link: "https://open.spotify.com/episode/3MRTqorDC3MTzm9TaLuvPc?si=oFyX1jVKQbyp0eyn3dzNnA"
+            },
+            {
+                title: "EMDR Attachment Focused",
+                link: "https://open.spotify.com/episode/3Ymfo7kbyKlnK3eNXplzEs?si=j10DjcwQTh2tHO1vmHwO8g"
+            },
+            {
+                title: "Polyamory and Consensual Non - Monogamy",
+                link: "https://open.spotify.com/episode/0TuuIt8ORVDHiKY2kobFSp?si=lV0QdlU2SAeeqs1mb_7x1w"
+            },
+            {
+                title: "Dreams: What do they really mean ?",
+                link: "https://open.spotify.com/episode/2fNgXV7Krvi5NQoJpCEJ61?si=CEj1vALmRQSqDtbKxhOY4Q",
+            },
+            {
+                title: "Expanded Podcast To Be Magnetic",
+                link: "https://open.spotify.com/episode/7u4xmFkHkd7haBv4UPtIbA?si=IJt69zPJTeWwT6WCX31Gaw"
+            }
+        ]  
+    },
+    books: {
+        trauma: [
+            'When the body Says No - Gabor Mate',
+            'Waking the Tiger - Peter Levine',
+            'The Body Keeps The Score - Besser Van Der Kolk',
+            'It Didn\'t Start with you -  Mark Wylon',
+            'In an Unspoken Voice - Peter Levine',
+            'The Myth of Normal: Trauma, Illness & Healing in a Toxic Culture - Gabor Mate and Daniel Mate'
+        ],
+        relationships: [
+            'Getting the love you want - Harville Hendrix',
+            'How We Love - Milan & Kay Yerkovich',
+            'Hold Me Tight - Sue Johnson',
+            'Mating in Captivity - Esther Perel',
+            'Conscious Uncoupling - Katherine Thomas',
+            'Attached - Amir Levine',
+            'Come as you are - Emily Nagoski',
+            'Co - Dependent No More - Melody Beattie',
+            'Facing Co - Dependency - Pia Melody',
+            'The Wizard of Oz & Other Narcissists I knew - Eleanor Payson',
+            'Will I Ever Be Good Enough - Karyl McBride'
+        ],
+        parenting: [
+            'The Whole Brain Child - Daniel Seigel',
+            'The Awakened Family - Shafali Tsabary',
+            'How We Love Our Kids - Milan & Kay Yerkovich',
+            'The Child Whisperer - Carol Tuttle',
+            'The Spirited Child - Mary Kurcinka',
+            'Trauma Proofing Your Kids - Peter Levine and Maggie Kline',
+            'Love & Logic - Jim Fay & Charles Fay'
+        ],
+        emotionalHealth: [
+            'The Gifts of Imperfection - Brene Brown',
+            'The Wild Edge of Sorrow - Francis Weller',
+            'How to Do the Work - Nicole LePera',
+            'Women Who Run with the Wolves - Clarissa Pinkola Estes',
+            'Letting Go - David Hawkins',
+            'Emotional Agility - Susan David',
+            'The Highly Sensitive Person - Elaine Aron',
+            'Energetic Boundaries - Cindi Dale',
+            'Empath\'s Survival Guide - Judith Orloff'
+        ]
+    },
+    contact: {
+        phone: '646-283-3561',
+        email: 'dr.alexandradewaal@gmail.com',
+        office: '48 Burd Street Suite 206 Nyack, NY 10960'
+    }
+},
+
+
+
+PODCASTS
+Attachment Theory
+Explaining Trauma
+EMDR Attachment Focused
+Polyamory and Consensual Non - Monogamy
+Dreams: What do they really mean ?
+    Expanded Podcast To Be Magnetic
+
+BOOKS
+Trauma
+When the body Say’s No - Gabor Mate
+Waking the Tiger - Peter Levine
+The Body Keeps The Score - Besser Van Der Kolk
+It Didn't Start with you -  Mark Wylon
+In an Unspoken Voice - Peter Levine
+The Myth of Normal: Trauma, Illness & Healing in a Toxic Culture - Gabor Mate and Daniel Mate
+Relationships
+Getting the love you want - Harville Hendrix
+How We Love - Milan & Kay Yerkovich
+Hold Me Tight - Sue Johnson
+Mating in Captivity - Esther Perel
+Conscious Uncoupling - Katherine Thomas
+Attached - Amir Levine
+Come as you are - Emily Nagoski
+Co - Dependent No More - Melody Beattie
+Facing Co - Dependency - Pia Melody
+The Wizard of Oz & Other Narcissists I knew - Eleanor Payson
+Will I Ever Be Good Enough - Karyl McBride
+Parenting
+The Whole Brain Child - Daniel Seigel
+The Awakened Family - Shafali Tsabary
+How We Love Our Kids - Milan & Kay Yerkovich
+The Child Whisperer - Carol Tuttle
+The Spirited Child - Mary Kurcinka
+Trauma Proofing Your Kids - Peter Levine and Maggie Kline
+Love & Logic - Jim Fay & Charles Fay
+Emotional health
+The Gifts of Imperfection - Brene Brown
+The Wild Edge of Sorrow - Francis Weller
+How to Do the Work - Nicole LePera
+Women Who Run with the Wolves - Clarissa Pinkola Estes
+Letting Go - David Hawkins
+Emotional Agility - Susan David
+The Highly Sensitive Person - Elaine Aron
+Energetic Boundaries - Cindi Dale
+Empath's Survival Guide - Judith Orloff
+Cost and Info
+Insurance: I am an out - of - network provider and you may be reimbursed in full or in part.I provide an invoice or receipt that you can submit for out - of - network reimbursement.A good way to start is to see if your plan offers any out - of - network benefits and at what percent the plan covers(so for example, if your weekly therapy cost is $250 and your insurance offers 80 % out - of - network reimbursement, then your cost after they reimburse you would be $50.00 per session).
+Rates: $250(sliding scale $250 - $155 income dependent)
+Payment - Cash, check, venmo, zelle, and all major credit cards accepted for payment.
+    Teletherapy - I offer in -person sessions and virtual sessions for New York residents only.I use a secure HIPAA platform to conduct these video sessions which you can access on your computer, tablet, or phone.
+
+        Contact
+Alexandra de Waal, Psy.D.
+    p. 646 - 283 - 3561
+e.dr.alexandradewaal@gmail.com
+Office: 48 Burd Street Suite 206 Nyack, NY 10960
+
+
+
+
+}
